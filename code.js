b9e1d4feb312eb6adaa063ecd21a477463263bac
canab <canab.ua@gmail.com>
2022-07-11 14:43:20 +0300

    use figma theme colors

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { themeColors: true });
 figma.ui.onmessage = msg => {
     switch (msg.type) {
         case "on_input":
